e2212948b3011920bc2ddc42d63574ba51bf3198
tbyrne_imag <tom.byrne@imagination.com>
2022-03-01 15:27:24 +1100

    Added JSON implementation

--- src_backend/types/extendscript.d.ts
+++ src_backend/types/extendscript.d.ts
@@ -2708,18 +2708,4 @@ type Pick<T, K extends keyof T> = { [P in K]: T[P] }
 type Record<K extends string, T> = { [P in K]: T }
 
 
-interface JSON {
 
-  /**
-   * The JSON.stringify() method converts a JavaScript object or value to a JSON string, optionally
-   * replacing values if a replacer function is specified or optionally including only the specified
-   * properties if a replacer array is specified.
-   */
-  stringify(obj:unknown, replacer?:unknown, space?:string): string | undefined
-
-  /**
-   * The JSON.parse() method parses a JSON string, constructing the JavaScript value or object described by the string.
-   */
-  parse<T>(str: string) : T
-}
-declare const JSON: JSON
