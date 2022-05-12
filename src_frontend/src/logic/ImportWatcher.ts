91cd33deb8c511d16e4452b903bece360be941aa
Tom Byrne <tom.byrne@imagination.com>
2022-05-12 16:22:01 +1000

    Improved import readiness check

--- src_frontend/src/logic/ImportWatcher.ts
+++ src_frontend/src/logic/ImportWatcher.ts
@@ -1,12 +1,12 @@
 import model from "@/model";
 import fs from "fs";
 import path from "path";
-import { ReadinessState, SpeakerItem } from "@/model/sequence";
-import SequenceTools from "./SequenceTools";
+import { ReadinessState, SpeakerItem, SpeakerRenderState } from "@/model/sequence";
+import SequenceTools, { findSpeakerItem } from "./SequenceTools";
 import glob from "glob";
 
 export function setup(): void {
-    setInterval(() => checkJobs(), 500);
+    setInterval(() => checkJobs(), 2000);
 }
 
 function updateJob(item: SpeakerItem, updates: Record<string, unknown>) {
@@ -28,16 +28,15 @@ function checkJobs() {
     const meta = model.sequence.sequenceMeta;
     if (!meta || !fs.existsSync(model.settings.pipeline_jobFolder)) return;
 
-    const allFiles = fs.readdirSync(model.settings.pipeline_jobFolder);
-
     for (const i in meta.speaker_items) {
-        const item = meta.speaker_items[i];
+        let item = meta.speaker_items[i];
 
         if (!fs.existsSync(item.import.asset_path)) {
             updateJob(item, { state: ReadinessState.NotReady });
         } else {
             const files = glob.sync(item.import.asset_path + "/*.jpg", { nodir: true });
-            if (files.length == item.render.total) {
+
+            if ((item.render.total && files.length == item.render.total) || (files.length && item.render.state == SpeakerRenderState.Done)) {
                 updateJob(item, { state: ReadinessState.Ready });
             } else {
                 updateJob(item, { state: ReadinessState.NotReady });
